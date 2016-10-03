9f5227ad0f5451cb99ee27ace65e6f5c1d640615
AlternateFiction <alt.fic@gmail.com>
2016-10-03 18:38:15 +0200

    Move frame creation to lua code

--- PingIdentifier.lua
+++ PingIdentifier.lua
@@ -1,23 +1,31 @@
 PingIdentifier = CreateFrame("Frame", nil, UIParent)
-PingIdentifier.Frame = PingIdentifierFrame
-PingIdentifier.FrameText = PingIdentifierFrameText
 LibStub("AceTimer-3.0"):Embed(PingIdentifier)
+local f, ft
 
 function PingIdentifier:OnInitialize()
     self.lastPingPlayerGUID = nil
     self.pingCount = {}
     self.pingText = ""
 
+    f = CreateFrame("Frame", "PingIdentifierFrame", MiniMapPing)
+    f:Hide()
+    f:SetFrameStrata("TOOLTIP")
+    f:SetBackdrop({bgFile="Interface\\Tooltips\\UI-Tooltip-Background", edgeFile="Interface\\Tooltips\\UI-Tooltip-Border", tile=true, edgeSize=16, tileSize=16, insets={left=5, right=5, top=5, bottom=5}})
+    f:SetBackdropBorderColor(TOOLTIP_DEFAULT_COLOR.r, TOOLTIP_DEFAULT_COLOR.g, TOOLTIP_DEFAULT_COLOR.b)
+    f:SetBackdropColor(TOOLTIP_DEFAULT_BACKGROUND_COLOR.r, TOOLTIP_DEFAULT_BACKGROUND_COLOR.g, TOOLTIP_DEFAULT_BACKGROUND_COLOR.b)
+    ft = f:CreateFontString("PingIdentifierFrameText", "ARTWORK", "GameFontNormalSmall")
+    ft:SetPoint("CENTER", 0, 0)
+
     self:SetScript("OnEvent", function (self, ...) self:OnEvent(...) end)
     self:RegisterEvent("MINIMAP_PING")
-    self.Frame:SetScale(0.8)
-    self.Frame:SetAlpha(1)
     self:UpdateScreen()
 end
 
 function PingIdentifier:UpdateScreen()
-    self.Frame:ClearAllPoints()
-    self.Frame:SetPoint("TOP", "Minimap", "BOTTOM", 0, 5)
+    f:ClearAllPoints()
+    f:SetPoint("TOP", "Minimap", "BOTTOM", 0, 5)
+    f:SetScale(0.8)
+    f:SetAlpha(1)
 end
 
 function PingIdentifier:OnEvent(event, pingPlayerGUID, ...)
@@ -29,21 +37,21 @@ function PingIdentifier:OnEvent(event, pingPlayerGUID, ...)
     end
     if not self.pingCount[pingPlayerGUID] then
         self.pingCount[pingPlayerGUID] = 1
-        self.FrameText:SetText(self.pingText)
+        ft:SetText(self.pingText)
     else
         self.pingCount[pingPlayerGUID] = self.pingCount[pingPlayerGUID] + 1
-        self.FrameText:SetText(self.pingText .. " |cffffffffx" .. self.pingCount[pingPlayerGUID])
+        ft:SetText(self.pingText .. " |cffffffffx" .. self.pingCount[pingPlayerGUID])
     end
-    self.Frame:SetWidth(self.FrameText:GetWidth() + 16)
-    self.Frame:SetHeight(self.FrameText:GetHeight() + 12)
-    self.Frame:Show()
+    f:SetWidth(ft:GetWidth() + 16)
+    f:SetHeight(ft:GetHeight() + 12)
+    f:Show()
     self:CancelAllTimers()
     self:ScheduleTimer("HidePingText", 5)
     self.lastPingPlayerGUID = pingPlayerGUID
 end
 
 function PingIdentifier:HidePingText()
-    self.Frame:Hide()
+    f:Hide()
     self.pingCount = {}
 end
 
